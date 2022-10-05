dbc28628cdaf4e12d1724fc7f05a494be6cb4319
Dieiverson Kava <38850216+Dieiverson@users.noreply.github.com>
2022-10-05 16:15:46 -0300

    Update checkout_screen.dart

--- projeto_hype/lib/screens/checkout_screen.dart
+++ projeto_hype/lib/screens/checkout_screen.dart
@@ -9,26 +9,24 @@ class CheckoutScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var uuid = Uuid();
+    var uuid = const Uuid();
     return Scaffold(
         appBar: AppBar(
           centerTitle: true,
           title: const Text("Checkout"),
           backgroundColor: const Color.fromARGB(255, 28, 184, 145),
         ),
-        body: Container(
-          child: Column(
-            children: [
-              const Text("Dados enviados servidor"),
-              Text("ID Usuário: ${uuid.v1()}"),
-              Text(jsonEncode(Cart().products)),
-              ElevatedButton(onPressed: (){
-                Cart().products = [];
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-                    builder: (context) => const HomeScreen()));
-              }, child: const Text("Voltar ao Início"))
-            ],
-          ),
+        body: Column(
+          children: [
+            const Text("Dados enviados servidor"),
+            Text("ID Usuário: ${uuid.v1()}"),
+            Text(jsonEncode(Cart().products)),
+            ElevatedButton(onPressed: (){
+              Cart().products = [];
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                  builder: (context) => const HomeScreen()));
+            }, child: const Text("Voltar ao Início"))
+          ],
         ));
   }
 }
